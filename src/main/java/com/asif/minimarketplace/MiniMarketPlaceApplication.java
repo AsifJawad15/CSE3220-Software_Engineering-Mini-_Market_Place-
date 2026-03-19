2c7f978ecc88460f36f12519babdf0535247c465
AsifJawad15 <asif@example.com>
2026-03-19 14:39:22 +0600

    chore: initial project setup - Spring Boot 3.5 skeleton

---
+++ src/main/java/com/asif/minimarketplace/MiniMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.asif.minimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarketPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asif/minimarketplace/MiniMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asif.minimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
