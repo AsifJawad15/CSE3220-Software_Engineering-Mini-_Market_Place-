7a5237892c4ff76977fa367b6e2e05976ec938cf
Arvi-saleque <alifsalek.as@gmail.com>
2026-04-02 12:51:36 +0600

    test: scaffold cart service tests class

---
+++ src/test/java/com/asif/minimarketplace/cart/service/CartServiceTest.java
@@ -0,0 +1,11 @@
+package com.asif.minimarketplace.cart.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CartServiceTest {
+    // Basic setup needed for next set of requests
+}
